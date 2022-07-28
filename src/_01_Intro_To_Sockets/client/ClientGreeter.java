51d711bf8483943265ffdb0a232bd030b6be1a0e
League Student <league@iMac-12.attlocal.net>
2022-07-28 11:41:56 -0700

    client greeter, server greeter
    check run

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -13,23 +13,27 @@ public class ClientGreeter {
 		// 2. Create an integer for the server's port number
 		int port = 8080;
 		// 3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
+		try {
 
-		// 4. Create an object of the Socket class. When initializing the object, pass
-		// in the ip address and the port number
+			// 4. Create an object of the Socket class. When initializing the object, pass
+			// in the ip address and the port number
+			Socket s = new Socket(ip, port);
+			// 5. Create a DataOutputStream object. When initializing it, use the Socket
+			// object you created in step 4 to call the getOutputStream() method.
+			DataOutputStream dOutput = new DataOutputStream(s.getOutputStream());
+			// 6. Use the DataOutputStream object to send a message to the server using the
+			// writeUTF(String message) method
+			dOutput.writeUTF("message to server");
+			// 7. Create a DataInputStream object. When initializing it, use the Server
+			// object you created in step 4 to call the getInputStream() method.
+			DataInputStream dInput = new DataInputStream(s.getInputStream());
+			// 8. Use the DataInputStream object to print a message from the server using
+			// the readUTF() method.
+			System.out.println(dInput.readUTF());
+			// 9. Close the client's server object
+			s.close();
+		} catch (IOException e) {
 
-		// 5. Create a DataOutputStream object. When initializing it, use the Socket
-		// object you created in step 4 to call the getOutputStream() method.
-
-		// 6. Use the DataOutputStream object to send a message to the server using the
-		// writeUTF(String message) method
-
-		// 7. Create a DataInputStream object. When initializing it, use the Server
-		// object you created in step 4 to call the getInputStream() method.
-
-		// 8. Use the DataInputStream object to print a message from the server using
-		// the readUTF() method.
-
-		// 9. Close the client's server object
-
+		}
 	}
 }
